c2ed4f45a99ab3d378a0a8f0ac3a3ca45b23fcb7
Lidia-Gomes-Code <lidiagomes.gb@gmail.com>
2024-02-29 17:54:29 -0300

    estrutura de decisao prontat

--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
-console.log("oi");
+// Parte 1: Criar tela de identificação do heroi no jogo.
+console.log("Digite o nome do seu heroi");
 
+// Declarar variavéis
+let nameheroi = " Mario "
+let xp = 11000
+
+// Tomada de Decisão
+let nheroi;
+
+if (nheroi < 1000){nheroi="Ferro"
+}else if( xp >= 1001 && xp <= 2000){
+nheroi="Bronze"
+}else {( xp >= 2001 && xp <= 5000)
+nheroi="Prata"
+};
+
+
+//Concatenando uma mensagem fixa + uma varável
+console.log("O Heroi de nome" + nameheroi + "está no nível de experiência " + nheroi);
+
+
+
+
+
+
+// 1 Desafio Classificador de nível de Herói
+
+// O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+//Objetivo
+
+//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+// Se XP for menor do que 1.000 = Ferro
+//Se XP for entre 1.001 e 2.000 = Bronze
+//Se XP for entre 2.001 e 5.000 = Prata
+//Se XP for entre 5.001 e 7.000 = Ouro
+//Se XP for entre 7.001 e 8.000 = Platina
+//Se XP for entre 8.001 e 9.000 = Ascendente
+//Se XP for entre 9.001 e 10.000= Imortal
+//Se XP for maior ou igual a 10.001 = Radiante
+
+//Saída
+
+//Ao final deve se exibir uma mensagem:
+//"O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+//"Iniciando Desafio"
+
+let name
